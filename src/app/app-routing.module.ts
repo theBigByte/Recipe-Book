2ece7a0f6c3adc9eb72f5c8e18c44a0faca726ab
theBigByte <bhardwaj.ritesh12@gmail.com>
2018-01-07 16:23:07 +0530

    Routing Module for App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,16 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 
+/* To define paths for the routing, we use 'Routes' as import from angular/router
+and define all the paths below as shown */
+
 const appRoutes : Routes = [
-    {path:'',redirectTo:'/recipes',pathMatch:'full'},
+    {path:'',redirectTo:'/recipes',pathMatch:'full'},//empty path should always use 'full' as pathMatch
     {path:'recipes',component:RecipesComponent, children:[
         {path:'',component:RecipeStartComponent, pathMatch:'full'},
         {path:':id',component:RecipeDetailComponent}
     ]},
+    
     {path:'shopping-list',component:ShoppingListComponent}
 ];
 
